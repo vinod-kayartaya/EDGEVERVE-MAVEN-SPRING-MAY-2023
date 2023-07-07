43998bbcad041e0b86e936f3f23a9b1ca59d0350
Vinod Kumar Kayartaya <vinod@vinod.co>
2023-07-07 12:58:52 +0530

    product-service queries

--- eclipse-projects/product-service/src/main/java/com/infosys/model/ProductDTO.java
+++ eclipse-projects/product-service/src/main/java/com/infosys/model/ProductDTO.java
@@ -1,8 +1,15 @@
 package com.infosys.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//@XmlRootElement(name="product")
 public class ProductDTO {
 	private Integer productId;
 	private String productName;
--- eclipse-projects/product-service/src/main/java/com/infosys/service/ProductService.java
+++ eclipse-projects/product-service/src/main/java/com/infosys/service/ProductService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.infosys.entity.Product;
@@ -15,18 +16,12 @@ public class ProductService {
 
 	@Autowired
 	private ProductRepository repo;
-	
+
 	private static ProductDTO getProductDTO(Product product) {
-		ProductDTO dto = new ProductDTO();
-		dto.setProductId(product.getProductId());
-		dto.setProductName(product.getProductName());
-		dto.setQuantityPerUnit(product.getQuantityPerUnit());
-		dto.setUnitPrice(product.getUnitPrice());
-		dto.setUnitsInStock(product.getUnitsInStock());
-		dto.setUnitsOnOrder(product.getUnitsOnOrder());
-		dto.setReorderLevel(product.getReorderLevel());
-		dto.setDiscontinued(product.getDiscontinued());
-		return dto;
+		return ProductDTO.builder().productId(product.getProductId()).productName(product.getProductName())
+				.quantityPerUnit(product.getQuantityPerUnit()).unitPrice(product.getUnitPrice())
+				.unitsInStock(product.getUnitsInStock()).unitsOnOrder(product.getUnitsOnOrder())
+				.reorderLevel(product.getReorderLevel()).discontinued(product.getDiscontinued()).build();
 	}
 
 	public ProductDTO getProductById(Integer productId) {
@@ -38,9 +33,59 @@ public class ProductService {
 		return getProductDTO(result.get());
 	}
 
-	
+	public List<ProductDTO> getAllProducts(Integer pageNum, Integer pageSize) {
 
-	public List<Product> getAllProducts(){
-		return repo.findAll();
+		PageRequest pr = PageRequest.of(pageNum - 1, pageSize);
+
+		return repo.findAll(pr) // Page<Product>
+				.stream() // Stream<Product>
+				.map(ProductService::getProductDTO) // Stream<ProductDTO>
+				.toList(); // List<ProductDTO>
+	}
+
+	public List<ProductDTO> searchByField(String fieldName, String fieldValue) {
+		List<Product> list = null;
+
+		switch (fieldName) {
+		case "unitPrice":
+			list = repo.findAllByUnitPrice(Double.valueOf(fieldValue));
+			break;
+		case "unitsInStock":
+			list = repo.findAllByUnitsInStock(Integer.valueOf(fieldValue));
+			break;
+		case "unitsOnOrder":
+			list = repo.findAllByUnitsOnOrder(Integer.valueOf(fieldValue));
+			break;
+		case "reorderLevel":
+			list = repo.findAllByReorderLevel(Integer.valueOf(fieldValue));
+			break;
+		case "discontinued":
+			list = repo.findAllByDiscontinued(Integer.valueOf(fieldValue));
+			break;
+		case "quantityPerUnit":
+			list = repo.findAllByQuantityPerUnit(fieldValue);
+			break;
+		case "productName":
+			list = repo.findAllByProductName(fieldValue);
+			break;
+		}
+
+		return list.stream().map(ProductService::getProductDTO).toList();
+	}
+
+	public List<ProductDTO> getByPriceRange(Double min, Double max) {
+		List<Product> list = repo.findAllByUnitPriceBetween(min, max);
+		return list.stream().map(ProductService::getProductDTO).toList();
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
